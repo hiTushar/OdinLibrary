639b3a805f63369f579d6c6e70b68d25d845fdfb
hiTushar <tusharjain2201@gmail.com>
2021-04-06 14:05:21 +0530

    updating progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) { // the constructor
+function Book(title, author, pages, status) { // the constructor
     this.title = title;
     this.author = author; 
     this.pages = pages; 
@@ -10,10 +10,20 @@ function Book(title, author, pages, read) { // the constructor
 function addBookToLibrary() {
     let x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
     myLibrary.push(x);
+     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
+    myLibrary.push(x);
+     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
+    myLibrary.push(x);
+     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
+    myLibrary.push(x);
+     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
+    myLibrary.push(x);
 }
 
 function display(){
     addBookToLibrary(); 
+    console.log(myLibrary);
+    document.querySelector('main').innerHTML = '';
     for(let book of myLibrary){
         let card = `<div class='card'>
                         <h1 class="title">${book.title}</h1>
@@ -26,9 +36,32 @@ function display(){
                     </div> 
                    `;
 
-        document.querySelector('main').innerHTML = card;
+        document.querySelector('main').innerHTML += card;
 
     }
 }
 
 display();
+
+/* form modal window */ 
+let addButton = document.querySelector('.add_books'); 
+let closeForm = document.querySelector('span.close'); 
+let modalForm = document.querySelector('.modal');
+
+addButton.onclick = function() {
+    modalForm.style.display = 'block'; 
+}
+
+closeForm.onclick = function() {
+    modalForm.style.display = 'none'; 
+}
+
+window.onclick = function(event) {
+    if(event.target === modalForm) {
+        modalForm.style.display = 'none'; 
+    }
+}
+
+Array.from(document.querySelectorAll('.modal-body input')).forEach(node => node.onchange = function(event) {
+                                                            console.log(event.target.id); 
+                                                        }); 
