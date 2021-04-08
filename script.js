c2ccd90a95bf3cbc5789867b97e2ae500b803ad8
hiTushar <tusharjain2201@gmail.com>
2021-04-08 09:02:59 +0530

    add progress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+
 
 function Book(title, author, pages, status) { // the constructor
     this.title = title;
@@ -7,49 +7,79 @@ function Book(title, author, pages, status) { // the constructor
     this.status = status; 
 }
 
-function addBookToLibrary() {
-    let x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
-    myLibrary.push(x);
-     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
-    myLibrary.push(x);
-     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
-    myLibrary.push(x);
-     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
-    myLibrary.push(x);
-     x = new Book('Harry Potter', 'J.K Rowling', 345, false); 
-    myLibrary.push(x);
+function saveCollection(libArr) {
+    window.localStorage.setItem('collection', JSON.stringify(libArr));
+}
+function getCollection() {
+    return JSON.parse(window.localStorage.getItem('collection')); 
+}
+
+function addBookToLibrary(obj) {
+    let myLibrary = getCollection() ? getCollection() : []; 
+    myLibrary.push(obj); 
+    saveCollection(myLibrary); 
 }
 
 function display(){
-    addBookToLibrary(); 
-    console.log(myLibrary);
+    let myLibrary = getCollection(); 
     document.querySelector('main').innerHTML = '';
-    for(let book of myLibrary){
-        let card = `<div class='card'>
-                        <h1 class="title">${book.title}</h1>
+    for(let [index, book] of myLibrary.entries()){
+        let card = `<div class='card' data-index='${index}'>
+                        <h1 class="title">${book.status?'':'*'}${book.title}</h1> <!-- adding * for unfinished book -->
                         <p class="data">
                             <span>${book.author}</span>
                             -
                             <span>${book.pages}p</span>
-                            <span>${book.status}</span>
                         </p>
+                        <div>
+                            <button id="del">Delete</button>
+                            <button class="change_status"></button>
+                        </div>
                     </div> 
                    `;
+        // document.querySelector('main').innerHTML += card;
+        document.querySelector('main').insertAdjacentHTML('beforeend', card);
 
-        document.querySelector('main').innerHTML += card;
+
+        if(book.status) { // book is finished i.e. checkbox is checked
+            document.querySelector(`div[data-index='${index}']`).className = 'card finished_shadow';
+            document.querySelector(`div[data-index='${index}'] button.change_status`).innerText = 'Read Again?'; 
+        }
+        else {
+            document.querySelector(`div[data-index='${index}']`).className = 'card unfinished_shadow';
+            document.querySelector(`div[data-index='${index}'] button.change_status`).innerText = 'Finished?'; 
+        }  
+        document.querySelector(`div[data-index='${index}'] button.change_status`).addEventListener('click', () => {changeStatus(index)});
+        
 
     }
 }
 
-display();
+function changeStatus(index) { // toggle finished/unfinished status
+    
+    let myLibrary = getCollection(); 
+    let myBook = myLibrary[index]; 
+    if(myBook.status) {
+        myBook.status = false; 
+    }
+    else {
+        myBook.status = true; 
+    }
+    saveCollection(myLibrary); 
+    display(); 
+}
+
 
 /* form modal window */ 
 let addButton = document.querySelector('.add_books'); 
 let closeForm = document.querySelector('span.close'); 
 let modalForm = document.querySelector('.modal');
+let myForm = document.getElementById('formElem'); 
 
 addButton.onclick = function() {
     modalForm.style.display = 'block'; 
+    myForm.style.display = 'block'; 
+    document.querySelector('.message').style.display = 'none'; 
 }
 
 closeForm.onclick = function() {
@@ -62,6 +92,23 @@ window.onclick = function(event) {
     }
 }
 
-Array.from(document.querySelectorAll('.modal-body input')).forEach(node => node.onchange = function(event) {
-                                                            console.log(event.target.id); 
-                                                        }); 
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let {title, author, pages, status} = myForm.elements; 
+
+    addBookToLibrary(new Book(title.value, author.value, pages.value, status.checked));
+    display();
+
+    e.target.reset(); 
+    myForm.style.display = 'none'; 
+    document.querySelector('.message').style.display = 'block';
+
+  });
+/* form modal window */ 
+
+ 
+
+
+  window.onload = () => display();
+ 
